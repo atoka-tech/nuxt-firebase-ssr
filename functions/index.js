063927a5882207ac7376a6ad9f7ef3c93baf8792
kota.toda <kota.toda@i-studio.co.jp>
2019-10-02 21:33:26 +0900

    feat: update nuxt.js

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,12 @@
 const functions = require('firebase-functions');
 const { Nuxt } = require('nuxt');
 
-const nuxt = new Nuxt({buildDir: 'ssr', dev: false});
+const nuxt = new Nuxt({
+  buildDir: 'ssr', // ssrのためのコード格納先
+  dev: false // buildしたものを使うので必要
+});
 
-exports.ssr = functions.https.onRequest(nuxt.render);
+exports.ssr = functions.https.onRequest(async (req, res) => {
+  await nuxt.ready() // since nuxt 2.5
+  return nuxt.render(req, res)
+})
