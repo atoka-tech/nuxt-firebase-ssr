74845bfaf5ba4f8055d811ff495a948200b9a378
kota.toda <kota.toda@i-studio.co.jp>
2020-03-14 17:43:25 +0900

    docs: a bit refactoring

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,10 @@
-const functions = require('firebase-functions');
-const { Nuxt } = require('nuxt');
+const functions = require('firebase-functions')
+const { Nuxt } = require('nuxt')
 
 const nuxt = new Nuxt({
-  buildDir: 'ssr', // ssrのためのコード格納先
+  buildDir: 'ssr',
   dev: false // buildしたものを使うので必要
-});
+})
 
 exports.ssr = functions.https.onRequest(async (req, res) => {
   await nuxt.ready() // since nuxt 2.5
